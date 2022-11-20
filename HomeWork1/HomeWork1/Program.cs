e37621f4ae5977e1d8baa7d6e272be5aa9eb7a55
Shagrid <shagrid@yandex.ru>
2022-11-20 10:42:29 +0300

    HW1 Complited

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -4,32 +4,80 @@ namespace HomeWork1
 {
     class Program
     {
+        private const string ExitSymbol = "q"; 
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуйте вас приветствует математическая программа");
-            Console.WriteLine("пажалуйста введите число. ");
-
-            String S = Console.ReadLine();
-
-            if (S == "q"){
-                return;
-            }
-            int M = Int32.Parse(S);
-            int c1 = 1; int c2 = 0;
-            int c3 = 0;
-            for (int i = 1; i <= M; i++)
+            try
             {
-                c1 = c1*i;
-                c2 = c2 + i;
-                if (i%2 == 0)
+                if (GetNumFromConsole(out var number))
                 {
-                    c3 = i;
+                    var factorial = GetFactorial( number );
+                    var sum = GetSumFrom1ToN( number );
+                    var maxEvenNumber = GetMaxEvenNumber( number );
+                    
+                    Console.WriteLine( $"Факториал равен {factorial}");
+                    Console.WriteLine( $"Сумма от 1 до N равна {sum} ");
+                    Console.WriteLine( $"Максимальное четное число меньше N равно { maxEvenNumber}");
                 }
             }
-            Console.WriteLine("Факториал равет " + c1); Console.WriteLine("Сума от 1 до N равна " + c2);
-
-            Console.WriteLine("максимальное четное число меньше N равно" + c3);
+            catch (Exception e)
+            {
+               Console.WriteLine("Видимо что-то служилось. Программа закрывается \r Обратитесь к системному администратору!");
+            }
             Console.ReadLine();
+        }
+
+        private static int GetMaxEvenNumber(int number)
+        {
+            var maxEvenNumber = 0;
+
+            for ( var i = 1; i < number; i++ )
+            {
+                if ( i % 2 == 0 )
+                {
+                    maxEvenNumber = i;
+                }
+            }
+            return maxEvenNumber;
+        }
+
+        private static int GetSumFrom1ToN(int number)
+        {
+            var sum = 0;
+
+            for ( var i = 1; i <= number; i++ )
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        private static int GetFactorial(int number)
+        {
+            var factorial = 1;
+            for ( var i = 1; i <= number; i++ )
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        private static bool GetNumFromConsole(out int number)
+        {
+            Console.WriteLine("Здравствуйте, Вас приветствует математическая программа.");
+            Console.WriteLine($"Пожалуйста, введите число. Для выхода введите {ExitSymbol}");
+            var inputData = Console.ReadLine();
+            while(!int.TryParse(inputData, out number))
+            {
+                if ( inputData == ExitSymbol )
+                {
+                    number = 0;
+                    return false;
+                }
+                Console.WriteLine( $"Вы ввели неправильное значение. Пожалуйста, введите именно число. Для выхода введите {ExitSymbol}");
+                inputData = Console.ReadLine();
+            }
+            return true;
         }
     }
 }
